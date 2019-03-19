b1219ca6c5a88338517be9ec10f4c43b1f9eb076
Dino <papaleguas95.19@gmail.com>
2019-03-18 22:03:22 -0300

    Projeto inicial

---
+++ dev_web/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import Menu from './menu/menu';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Menu />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ dev_web/src/menu/menu.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Menu extends Component{
+    
+    render(){
+        return(
+            <div>
+                <label>Ola mundo</label>
+            </div>
+        );
+    }
+}
+export default Menu;
+
